301fcb7867376806ffadd5454524a60f80b2ab13
Pasquale Kress <wabisabisf@gmail.com>
2017-12-06 22:52:07 +0100

    basic solution with only small changes

---
+++ challenges/01_Beginnings/solution.rb
@@ -0,0 +1,41 @@
+astrix = "************************"     # unless you define several methods,
+                                        # there is no need to declare global variables
+puts astrix
+puts "Welcome to my dice simulator"
+puts astrix
+
+randomdie = (1..6).to_a                       # there is really no need to make an array out of this variable
+                                              # ..or to even store it in a variable in the first place
+randomedieresult = randomdie.sample           # now you have a random Integer
+
+puts "in our computer simulation we roll a 6 sided die and it rolled #{randomedieresult}"
+                                              # this only works because of string interpolation, normally you
+                                              # would have to convert
+                                              # your Integer (called Fixnum in Ruby) into a string
+
+puts "what number did you roll?"
+dice = gets.chomp.to_i
+
+puts "roll your dice 5 times"
+puts astrix
+puts "press enter to roll 5 times"
+roll = gets.chomp
+randomedieresult = randomdie.sample
+
+for i in (1..5) do
+  puts randomdie.sample                             # wrong variable name
+end
+
+
+puts "write your results in order rolled"     # ok, I see what you are trying to do here, nice
+roll5 = gets.chomp.to_s
+numbers = [roll5].sort                        # what type of variable are you creating here,
+reversednumbers = roll5.reverse!
+big5 = numbers.last
+puts "you got #{numbers} or #{reversednumbers} (reveresed) and your highest number was #{big5}"
+                                              # I like the idea, let's see how we can get this done
+
+  # else puts "something mustve went wrong"   # because of that error on line 15 you don't even get here,
+                                              # but if that error was not there, what would be the point o
+                                              # point out (like... the number you have entered i                                              # something like that)
+# end
