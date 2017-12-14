92f860ff80b2df56aa139981d7939e88859d6f16
noobieruby <louishunter49@gmail.com>
2017-12-13 23:30:19 -0800

    work in progress

--- challenges/04_Data_Types/state-capital.rb
+++ challenges/04_Data_Types/state-capital.rb
@@ -1,9 +1,8 @@
-puts "type a state"
-hello = gets.chomp
-hello.capitilize
+congradulation = ["Keep it up", "Good work!","Well done!","Your a genius","Amazing","Good job!", "Your a rockstar", "Excellent","Keep up the good work"]
 
+mistake = ["ahh, almost, keep on trying","good try keep it up","thats not the correct answer but keep it going","wrong answer but you got this!"]
 
-capitials = {"Alabama"=>"Montgomery",
+statesCapitals_hash = {"Alabama"=>"Montgomery",
  "Alaska"=>"Juneau",
  "Arizona"=>"Phoenix",
  "Arkansas"=>"Little Rock",
@@ -53,3 +52,68 @@ capitials = {"Alabama"=>"Montgomery",
  "West Virginia"=>"Charleston",
  "Wisconsin"=>"Madison",
  "Wyoming"=>"Cheyenne"}
+
+input = ""
+input2 = ["Montgomery","Juneau","Phoenix","Little Rock", "Sacramento","Denver","Hartford","Dover","Tallahassee",
+"Atlanta","Honolulu","Boise","Springfield","Indianapolis","Des Moines","Topeka","Frankfort","Baton Rouge","Augusta",
+"Annapolis","Boston","Lansing","St. Paul","Jackson","Jefferson City", "Helena","Lincoln","Carson City","Concord",
+"Trenton","Santa Fe","Albany","Raleigh","Bismarck","Columbus","Oklahoma City","Salem","Harrisburg","Providence",
+"Columbia","Pierre","Nashville","Austin","Salt Lake City","Montpelier","Richmond","Olympia","Charleston","Madison",
+"Cheyenne"]
+
+capitals = ""
+
+
+puts "********************************************************************************
+                          Welcome to my Capital Finder"
+ask = "no"
+
+while ask != "n"
+1.times {print "
+Type a State: "
+input = gets.chomp.capitalize
+
+if statesCapitals_hash.include?(input) then
+  capitals = statesCapitals_hash[input]
+  puts "
+#{capitals} is #{input}'s captial"
+else "that does not seem to be a state"
+end}
+print "
+do you want to find another capital? [Y/N]: "
+goodbyecruelworld = gets.chomp
+if goodbyecruelworld == ""
+  ask = "y"
+else
+  ask = goodbyecruelworld.downcase
+end
+end
+
+
+
+print "
+Would you like to play a capital trivia? [Y/N]:  "
+yes = gets.chomp.upcase
+
+if yes == "Y"
+
+for i in(1..10)
+  random = input2.sample
+  puts "
+  #{random} is what states capital?"
+  trivia = gets.chomp.capitalize
+  statesCapitals_hash.include?(trivia)
+  answer = statesCapitals_hash[trivia]
+
+if answer == random
+    puts "#{congradulation.sample}"
+  else puts "#{mistake}"
+  end
+end
+puts "good work"
+
+else puts "thanks for using this tool"
+end
+
+print "
+********************************************************************************"
