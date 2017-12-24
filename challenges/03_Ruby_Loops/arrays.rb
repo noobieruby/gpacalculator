a68b0f61ac15e97dbb67685b1a8975de232fda4c
Pasquale Kress <wabisabisf@gmail.com>
2017-12-24 22:22:27 +0100

    typos matter

--- challenges/03_Ruby_Loops/arrays.rb
+++ challenges/03_Ruby_Loops/arrays.rb
@@ -4,14 +4,16 @@ array = ["random", "something", "green", "red", 23, 54, 555, 1, ["more stuff", 1
 puts "What do you want to search for?"
 find = gets.chomp
 
+answerPositive = []
+
 array.each do |element|
-if element == find
-  answerPositive == "yes"
-else print ""
-end
+  if element == find
+    answerPositive << 1
+  end
 end
 
-if answerPostive.include?("yes")
+if answerPositive.include?(1)
   puts "the array includes #{find}"
-else puts "sorry the array doesnt include #{find}"
+else
+  puts "sorry the array doesnt include #{find}"
 end
