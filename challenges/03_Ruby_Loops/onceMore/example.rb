a530541745062e176d73dbd61f8b122d01a968f7
Pasquale Kress <wabisabisf@gmail.com>
2017-12-31 01:26:05 +0100

    this is a mask if you need help

---
+++ challenges/03_Ruby_Loops/onceMore/example.rb
@@ -0,0 +1,19 @@
+def total_for(min, max)
+  sum = 0
+  for i in  # Your Code Here
+    # ==>
+  end
+  return sum
+end
+
+def total_while(min, max)
+  sum = 0
+  while # Your Code Here
+    # ==>
+  end
+  return sum
+end
+
+# You can put some example values in these brackets
+# to try if your code works
+puts total_while()
